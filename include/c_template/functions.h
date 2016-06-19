ee25e9290db1d4a13b8fa48ae35c3733d19ec132
Melvin Vermeeren <mail@mel.vin>
2016-06-20 01:13:06 +0200

    Add CMake architecture (C, C++) and simple C application

---
+++ include/c_template/functions.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+uint8_t return_zero(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/functions.c
@@ -0,0 +1,6 @@
+#include <c_template/functions.h>
+
+uint8_t return_zero(void)
+{
+	return 0;
+}
