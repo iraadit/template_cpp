1af37ef3a3e1e9b0e8bda3fe6549cfef6002ec3b
Melvin Vermeeren <mail@mel.vin>
2018-01-28 17:59:42 +0100

    add CLANG_TIDY and WERROR flags

---
+++ cmake/clang_tidy.cmake
@@ -0,0 +1,23 @@
+# find clang-tidy
+find_program(CLANG_TIDY_PATH
+	clang-tidy
+	DOC "Path to clang-tidy."
+)
+
+# error if not found
+if(NOT CLANG_TIDY_PATH)
+	message(FATAL_ERROR "Could not find clang-tidy.")
+endif()
+message(STATUS "Path to clang-tidy: ${CLANG_TIDY_PATH}")
+
+# set clang-tidy options, support WERROR
+set(OPT "${CLANG_TIDY_PATH}")
+if(WERROR)
+	set(OPT "${OPT};-warnings-as-errors=*")
+endif()
+
+# set default properties for all new targets
+set(CMAKE_C_CLANG_TIDY "${OPT}" CACHE STRING
+	"Clang-tidy options for C." FORCE)
+set(CMAKE_CXX_CLANG_TIDY "${OPT}" CACHE STRING
+	"Clang-tidy options for C++." FORCE)
