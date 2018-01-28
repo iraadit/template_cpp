ed3bf3c430384e0d89c6466f475534186795332c
Rowan Goemans <RB.Goemans@student.han.nl>
2018-01-28 22:06:02 +0100

    -- Fixed additional review points.

--- cmake/code_coverage.cmake
+++ cmake/code_coverage.cmake
@@ -1,4 +1,4 @@
-# Check prereqs
+# check prereqs
 find_program(GCOV_PATH gcov)
 if(NOT GCOV_PATH)
 	message(FATAL_ERROR "gcov not available...")
@@ -19,37 +19,35 @@ if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
 		"Code coverage is only supported for CMAKE_BUILD_TYPE=DEBUG.")
 endif()
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-message(STATUS 
-	"Appending code coverage compiler flags: --coverage")
+set(CMAKE_EXE_LINKER_FLAGS "--coverage" CACHE INTERNAL "" FORCE)
+add_compile_options(--coverage)
 
-# Reset coverage
+# clear coverage
 add_custom_target(coverage_clear
-	# Cleanup lcov
 	COMMAND ${LCOV_PATH} --directory . --zerocounters
-	COMMENT "Resetting coverage."
+	COMMENT "Clearing coverage."
 )
 
-# Setup target
+# setup target
 add_custom_target(coverage_report
-	# Create baselines to make sure untouched files show up in the report
+	# create baselines to make sure untouched files show up in the report
 	COMMAND ${LCOV_PATH} -q -c -i -d 
 		. -o coverage_report.base
-	# Capturing lcov counters and generating report
+	# capturing lcov counters and generating report
 	COMMAND ${LCOV_PATH} -q --directory . --capture 
 	--output-file coverage_report.info
 
-	# Add baseline counters
+	# add baseline counters
 	COMMAND ${LCOV_PATH} -q 
 		-a coverage_report.base -a coverage_report.info
 		--output-file coverage_report.total
 
-	# Exclude everything but the project sources.
+	# exclude everything but the project sources
 	COMMAND ${LCOV_PATH} -q
 		-e coverage_report.total "\"${CMAKE_SOURCE_DIR}/*\""
 		--output-file coverage_report.info.cleaned
-	COMMAND ${GENHTML_PATH} -o coverage_report coverage_report.info.cleaned
+	COMMAND ${GENHTML_PATH} 
+		-o coverage_report coverage_report.info.cleaned
 	COMMAND ${CMAKE_COMMAND} -E remove coverage_report.base
 		coverage_report.info coverage_report.all coverage_report.total
 		coverage_report.info.cleaned
@@ -59,7 +57,7 @@ add_custom_target(coverage_report
 	COMMENT "Handling coverages files and building html report."
 )
 
-# Show info where to find the report
+# show info where to find the report
 add_custom_command(TARGET coverage_report POST_BUILD
 	COMMAND ;
 	COMMENT "Report is available at ./coverage_report/index.html."
