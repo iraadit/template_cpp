7ca9aa8456668268403766f81fc008ad5ce3fa7b
Rowan Goemans <RB.Goemans@student.han.nl>
2018-01-28 20:11:59 +0100

    -- Fixed indentation in CodeCoverage.cmake

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -42,7 +42,8 @@
 # - Merged with modified version from github.com/ufz/ogs
 #
 # 2017-01-28, Rowan Goemans
-# - Removed the need to setup targets. Just enable coverage then run your targets the way you like and then create a report.
+# - Removed the need to setup targets. Just enable coverage then 
+#   run your targets the way you like and then create a report.
 #
 #
 # USAGE:
@@ -54,116 +55,122 @@
 #
 # 3. Build a Debug build, run it and create a report:
 #      cmake -DCMAKE_BUILD_TYPE=Debug ..
-#      [Compile and run the targets you want to generated coverage data for,]
+#      [Compile and run the targets you want to generated coverage data for]
 #      make coverage_report
 #
-# 4. Clear out coverage data to allow a clean slate if you want to check a specific target.
+# 4. Clear out coverage data, this resets all data to 0:
 #      make coverage_clear
 #
 
-include(CMakeParseArguments)
-
 # Check prereqs
-find_program( GCOV_PATH gcov )
-find_program( LCOV_PATH  NAMES lcov lcov.bat lcov.exe lcov.perl)
-find_program( GENHTML_PATH NAMES genhtml genhtml.perl genhtml.bat )
-find_program( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
-find_program( SIMPLE_PYTHON_EXECUTABLE python )
+find_program(GCOV_PATH gcov )
+find_program(LCOV_PATH  NAMES lcov lcov.bat lcov.exe lcov.perl)
+find_program(GENHTML_PATH NAMES genhtml genhtml.perl genhtml.bat )
+find_program(GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
+find_program(SIMPLE_PYTHON_EXECUTABLE python )
 
 if(NOT GCOV_PATH)
-    message(FATAL_ERROR "gcov not found! Aborting...")
+	message(FATAL_ERROR "gcov not found! Aborting...")
 endif() # NOT GCOV_PATH
 
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
-    if("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS 3)
-        message(FATAL_ERROR "Clang version must be 3.0.0 or greater! Aborting...")
-    endif()
-elseif(NOT "${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" AND NOT "${CMAKE_C_COMPILER_ID}" MATCHES "GNU")
-    message(FATAL_ERROR "Compiler is not GNU gcc! Aborting..." )
+	if("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS 3)
+		message(FATAL_ERROR 
+			"Clang version must be 3.0.0 or greater! Aborting...")
+	endif()
+elseif(NOT "${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" 
+	AND NOT "${CMAKE_C_COMPILER_ID}" MATCHES "GNU")
+	message(FATAL_ERROR "Compiler is not GNU gcc! Aborting..." )
 endif()
 
 set(COVERAGE_COMPILER_FLAGS "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
-    CACHE INTERNAL "")
+	CACHE INTERNAL "")
 
 set(CMAKE_CXX_FLAGS_COVERAGE
-    ${COVERAGE_COMPILER_FLAGS}
-    CACHE STRING "Flags used by the C++ compiler during coverage builds."
-    FORCE )
+	${COVERAGE_COMPILER_FLAGS}
+	CACHE STRING "Flags used by the C++ compiler during coverage builds."
+	FORCE )
 set(CMAKE_C_FLAGS_COVERAGE
-    ${COVERAGE_COMPILER_FLAGS}
-    CACHE STRING "Flags used by the C compiler during coverage builds."
-    FORCE )
+	${COVERAGE_COMPILER_FLAGS}
+	CACHE STRING "Flags used by the C compiler during coverage builds."
+	FORCE )
 set(CMAKE_EXE_LINKER_FLAGS_COVERAGE
-    ""
-    CACHE STRING "Flags used for linking binaries during coverage builds."
-    FORCE )
+	""
+	CACHE STRING "Flags used for linking binaries during coverage builds."
+	FORCE )
 set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
-    ""
-    CACHE STRING "Flags used by the shared libraries linker during coverage builds."
-    FORCE )
+	""
+	CACHE STRING 
+		"Flags used by the shared libraries linker during coverage builds."
+	FORCE )
 mark_as_advanced(
-    CMAKE_CXX_FLAGS_COVERAGE
-    CMAKE_C_FLAGS_COVERAGE
-    CMAKE_EXE_LINKER_FLAGS_COVERAGE
-    CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
+	CMAKE_CXX_FLAGS_COVERAGE
+	CMAKE_C_FLAGS_COVERAGE
+	CMAKE_EXE_LINKER_FLAGS_COVERAGE
+	CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
 
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
-    message(FATAL_ERROR "Code coverage results with an optimised (non-Debug) build may be misleading")
+	message(FATAL_ERROR 
+		"Code coverage results is only valid with debug builds.")
 endif() # NOT CMAKE_BUILD_TYPE STREQUAL "Debug"
 
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
-    link_libraries(gcov)
+	link_libraries(gcov)
 else()
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
+	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
 endif()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COVERAGE_COMPILER_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COVERAGE_COMPILER_FLAGS}")
-message(STATUS "Appending code coverage compiler flags: ${COVERAGE_COMPILER_FLAGS}")
+message(STATUS 
+	"Appending code coverage compiler flags: ${COVERAGE_COMPILER_FLAGS}")
 
 # defines target to create coverage.
 if(NOT LCOV_PATH)
-  message(FATAL_ERROR "lcov not found! Aborting...")
+	message(FATAL_ERROR "lcov not found! Aborting...")
 endif() # NOT LCOV_PATH
 
 if(NOT GENHTML_PATH)
-  message(FATAL_ERROR "genhtml not found! Aborting...")
+	message(FATAL_ERROR "genhtml not found! Aborting...")
 endif() # NOT GENHTML_PATH
 
 # Reset coverage
 add_custom_target(coverage_clear
-  # Cleanup lcov
-  COMMAND ${LCOV_PATH} --directory . --zerocounters
-  COMMENT "Resetting code coverage counters to zero."
+	# Cleanup lcov
+	COMMAND ${LCOV_PATH} --directory . --zerocounters
+	COMMENT "Resetting code coverage counters to zero."
 )
 
 # Setup target
 add_custom_target(coverage_report
-  # Create baselines to make sure untouched files show up in the report
-  COMMAND ${LCOV_PATH} -c -i -d . -o coverage_report.base
-  # Capturing lcov counters and generating report
-  COMMAND ${LCOV_PATH} --directory . --capture 
-    --output-file coverage_report.info
+	# Create baselines to make sure untouched files show up in the report
+	COMMAND ${LCOV_PATH} -c -i -d . -o coverage_report.base
+	# Capturing lcov counters and generating report
+	COMMAND ${LCOV_PATH} --directory . --capture 
+	--output-file coverage_report.info
 
-  # Add baseline counters
-  COMMAND ${LCOV_PATH} 
-    -a coverage_report.base -a coverage_report.info
-    --output-file coverage_report.total
+	# Add baseline counters
+	COMMAND ${LCOV_PATH} 
+		-a coverage_report.base -a coverage_report.info
+		--output-file coverage_report.total
 
-  # Exclude everything but the project sources.
-  COMMAND ${LCOV_PATH} 
-    -e coverage_report.total "\"${CMAKE_SOURCE_DIR}/*\""
-    --output-file coverage_report.info.cleaned
-  COMMAND ${GENHTML_PATH} -o coverage_report ${PROJECT_BINARY_DIR}/coverage_report.info.cleaned
-  COMMAND ${CMAKE_COMMAND} -E remove coverage_report.base coverage_report.info coverage_report.all coverage_report.total ${PROJECT_BINARY_DIR}/coverage_report.info.cleaned
+	# Exclude everything but the project sources.
+	COMMAND ${LCOV_PATH} 
+		-e coverage_report.total "\"${CMAKE_SOURCE_DIR}/*\""
+		--output-file coverage_report.info.cleaned
+	COMMAND ${GENHTML_PATH} 
+		-o coverage_report ${PROJECT_BINARY_DIR}/coverage_report.info.cleaned
+	COMMAND ${CMAKE_COMMAND} -E remove coverage_report.base
+		coverage_report.info coverage_report.all coverage_report.total
+		${PROJECT_BINARY_DIR}/coverage_report.info.cleaned
 
-  WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-  DEPENDS ${Coverage_DEPENDENCIES}
-  COMMENT "Processing code coverage counters and generating report."
+	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+	DEPENDS ${Coverage_DEPENDENCIES}
+	COMMENT "Processing code coverage counters and generating report."
 )
 
 # Show info where to find the report
 add_custom_command(TARGET coverage_report POST_BUILD
-  COMMAND ;
-  COMMENT "Open ./coverage_report/index.html in your browser to view the coverage report."
+	COMMAND ;
+	COMMENT "Report is available at ./coverage_report/index.html."
 )
