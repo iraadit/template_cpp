7267734cc2886baf7b87b930875ef5ae9b855453
Melvin Vermeeren <mail@mel.vin>
2018-01-29 18:50:33 +0100

    add catch to ci dockerfile

--- ci/docker_targets/ci/bootstrap.sh
+++ ci/docker_targets/ci/bootstrap.sh
@@ -6,12 +6,19 @@ apt-get update
 
 apt-get install -y \
 	clang-6.0 \
-	clang-tidy-6.0 \
 	clang-format-6.0 \
+	clang-tidy-6.0 \
 	cmake \
 	cppcheck \
-	gcc \
+	curl \
 	g++ \
+	gcc \
 	lcov
 
+mkdir -p /usr/local/include/catch
+curl -Lf \
+	-o /usr/local/include/catch/catch.hpp \
+	https://git.mel.vin/mirror/catch/raw/v2.1.1/catch/catch.hpp
+
+apt-get clean
 rm -rf /var/lib/apt/lists/*
