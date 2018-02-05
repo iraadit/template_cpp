0c0c0643b424c384daa26b3d55d3b437c332a5d0
Melvin Vermeeren <mail@mel.vin>
2018-02-05 15:24:04 +0100

    add plantuml to sphinx_html ci image

--- ci/docker_targets/sphinx_html/bootstrap.sh
+++ ci/docker_targets/sphinx_html/bootstrap.sh
@@ -5,15 +5,31 @@ set -e
 apk --no-cache add \
 	binutils \
 	cmake \
-	openjdk8-jre-base \
 	doxygen \
 	graphviz \
 	libc-dev \
 	make \
+	openjdk8-jre-base \
 	py2-pip
+
+apk --no-cache add -t tmp \
+	curl
 
 pip install \
 	breathe \
 	sphinx \
 	sphinx_rtd_theme \
-	sphinxcontrib.plantuml
+	sphinxcontrib-plantuml
+
+mkdir -p /opt/plantuml
+curl -Lf \
+	-o /opt/plantuml/plantuml.jar \
+	https://git.mel.vin/mirror/plantuml/raw/1.2018.1/plantuml.jar
+
+printf '%s\n%s\n' \
+	'#!/bin/sh' \
+	'exec java -jar /opt/plantuml/plantuml.jar "$@"' \
+	> /usr/local/bin/plantuml
+chmod +x /usr/local/bin/plantuml
+
+apk --no-cache del tmp
