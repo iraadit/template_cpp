90beff496d5e7aae9a2a72d36484a229c88bef9d
Melvin Vermeeren <mail@mel.vin>
2018-02-16 16:26:04 +0100

    add add(), subtract() example to main()

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
+#include <c_template/add.h>
+#include <c_template/subtract.h>
+
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+static const uint8_t A = 4;
+static const uint8_t B = 37;
 
 int main(int argc, char **argv)
 {
@@ -9,6 +16,11 @@ int main(int argc, char **argv)
 	for (i = 0; i < argc; ++i) {
 		printf("argv[%i]: %s\n", i, argv[i]);
 	}
+
+	printf("add(%" PRIu8 ", %" PRIu8 ") = %" PRIu16 "\n", A, B, add(A, B));
+
+	printf("subtract(%" PRIu8 ", %" PRIu8 ") = %" PRIi16 "\n", A, B,
+		subtract(A, B));
 
 	return EXIT_SUCCESS;
 }
