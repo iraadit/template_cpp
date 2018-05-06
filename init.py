3b3fff30e100eb3f302093c919f1521fdd31dad1
ximin <ximin@ximinity.net>
2018-05-06 22:20:01 +0200

    reset docker image version to 1.0 on init

--- init.py
+++ init.py
@@ -61,32 +61,27 @@ class InitTask(ABC):
                 pass
 
 
-class ReplaceTask(InitTask):
-        """ReplaceTask is the task for replacing strings in files"""
-        def __init__(self, filename, old_val, new_val):
+class ReReplaceTask(InitTask):
+        """ReReplaceTask is the task for replacement using regular expressions.
+        """
+        def __init__(self, filename, oregex, nval):
                 self._filename = filename
-                self._old_val = old_val
-                self._new_val = new_val
+                self._oregex = oregex
+                self._nval = nval
 
         def exec(self):
-                """Replaces the old value with the new value in the given
-                file.
+                """Replaces the strings matching the oregex with the new value.
                 """
                 with open(self._filename, "r+") as f:
                         s = f.read()
-                        if self._old_val not in s:
-                                raise Exception("%s not found in %s."
-                                                % (self._old_val,
-                                                   self._filename))
-
-                        s = s.replace(self._old_val, self._new_val)
+                        s = re.sub(self._oregex, self._nval, s)
                         f.seek(0)
                         f.write(s)
                         f.truncate()
 
 
 class DeleteTask(InitTask):
-        """ReplaceTask is the task for deleting files and directories"""
+        """DeleteTask is the task for deleting files and directories"""
         def __init__(self, filename):
                 self._filename = filename
 
@@ -114,40 +109,46 @@ print("Initializing the template.")
 
 # basic project info
 pname = def_input("Project name", prj_dir)
-taskq.put(ReplaceTask(cmakelist, "__PROJECT_NAME__", pname))
+taskq.put(ReReplaceTask(cmakelist, "__PROJECT_NAME__", pname))
 
 author = def_input("Project author", config['name'])
-taskq.put(ReplaceTask(cmakelist, "__AUTHOR__", author))
+taskq.put(ReReplaceTask(cmakelist, "__AUTHOR__", author))
 
 mail = def_input("Project email", config['email'])
-taskq.put(ReplaceTask(cmakelist, "__AUTHOR_MAIL__", mail))
+taskq.put(ReReplaceTask(cmakelist, "__AUTHOR_MAIL__", mail))
 
 version = def_input("Project version", "1.0.0")
-taskq.put(ReplaceTask(cmakelist, "0.0.0", version))
+taskq.put(ReReplaceTask(cmakelist, "0.0.0", version))
 
 copyright = def_input("Copyright", "%s, %s" %
                       (datetime.datetime.now().year,  author))
-taskq.put(ReplaceTask(cmakelist, "__COPYRIGHT__", copyright))
+taskq.put(ReReplaceTask(cmakelist, "__COPYRIGHT__", copyright))
 
 description = def_input("Description", prj_dir)
-taskq.put(ReplaceTask(cmakelist, "__DESCRIPTION__", description))
+taskq.put(ReReplaceTask(cmakelist, "__DESCRIPTION__", description))
 
 # gitlab ci
 use_ci = def_input("Use gitlab ci?", "yes")
 if use_ci == "yes":
         # get new registry url
+        ver_cur = ":[0-9]\.[0-9]"
+        ver_new = ":1.0"
         reg_domain = req_input("Docker registry domain")
         reg_port = req_input("Docker registry port")
         gitlab_group = req_input("Gitlab group/user name")
-        or_url = "git.mel.vin:5005/template/c"
-        url = "%s:%s/%s/%s" % (reg_domain, reg_port, gitlab_group, pname)
-        taskq.put(ReplaceTask(gitlabci, or_url, url))
+        org_url = "git.mel.vin:5005/template/c"
+        new_url = "%s:%s/%s/%s" % (reg_domain, reg_port, gitlab_group, pname)
+
+        # update urls
+        taskq.put(ReReplaceTask(gitlabci, org_url, new_url))
+        taskq.put(ReReplaceTask(gitlabci, ver_cur, ver_new))
 
         # update image registry location
         for root, dirs, files in os.walk(dockertarget):
                 for f in (file for file in files if file == "Tagfile"):
                         full_path = os.path.join(root, f)
-                        taskq.put(ReplaceTask(full_path, or_url, url))
+                        taskq.put(ReReplaceTask(full_path, org_url, new_url))
+                        taskq.put(ReReplaceTask(full_path, ver_cur, ver_new))
 else:
         taskq.put(DeleteTask(gitlabci))
 
