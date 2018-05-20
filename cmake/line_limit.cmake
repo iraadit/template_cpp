e45ab9f1a8f6b3751d9d90de4fcb481334836890
ximin <ximin@ximinity.net>
2018-05-20 21:33:17 +0200

    call line_limit through intermediate script

--- cmake/line_limit.cmake
+++ cmake/line_limit.cmake
@@ -1,15 +1,4 @@
 add_custom_target(line_limit
 	COMMENT "Running line_limit checks"
-	COMMAND ./ci/style/line_limit.sh
-		-e "'\\.(c|cpp|h|hpp)$$'"
-		-e "'^\\.clang-tidy$$'"
-		-e "'^cmake/apidoc\\.py$$'"
-		-e "'^build.*/'"
-		-i .
-	COMMAND ./ci/style/line_limit.sh
-		-e "'CMakeLists\\.txt$$'" include
-	COMMAND ./ci/style/line_limit.sh
-		-e "'CMakeLists\\.txt$$'" src
-	COMMAND ./ci/style/line_limit.sh
-		-e "'CMakeLists\\.txt$$'" test
+	COMMAND WERROR=${WERROR} ./cmake/line_limit.sh
 	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
