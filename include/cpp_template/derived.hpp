eb05d190e4ddd49cb4efef2926c97f99968680cb
Melvin Vermeeren <mail@mel.vin>
2018-06-11 22:00:13 +0200

    remove \brief since AUTOBRIEF=ON

--- include/cpp_template/derived.hpp
+++ include/cpp_template/derived.hpp
@@ -4,90 +4,90 @@
 #include <string>
 
 /**
- * \brief Defines some known class types.
+ * Defines some known class types.
  */
 enum class class_type
 {
 	/**
-	 * \brief Has no implementation.
+	 * Has no implementation.
 	 */
 	INTERFACE,
 
 	/**
-	 * \brief Has some implementation.
+	 * Has some implementation.
 	 */
 	ABSTRACT,
 
 	/**
-	 * \brief Has only implementation.
+	 * Has only implementation.
 	 */
 	NORMAL
 };
 
 /**
- * \brief struct containing a min and a max.
+ * struct containing a min and a max.
  */
 struct range
 {
 	/**
-	 * \brief The minimum value.
+	 * The minimum value.
 	 */
 	std::uint8_t min;
 
 	/**
-	 * \brief The maximum value.
+	 * The maximum value.
 	 */
 	std::uint8_t max;
 };
 
 /**
- * \brief Convert class_type to a string representation.
+ * Convert class_type to a string representation.
  * \param t The type to translate.
  * \return The string variant.
  */
 std::string class_types_to_string(class_type t);
 
 /**
- * \brief Class that derives from an abstract base
+ * Class that derives from an abstract base
  */
 class derived : public base
 {
 public:
 	/**
-	 * \brief Default constructor.
+	 * Default constructor.
 	 */
 	derived() = default;
 
 	~derived() override = default;
 
 	/**
-	 * \brief Default copy constructor
+	 * Default copy constructor
 	 * \param d
 	 */
 	derived(const derived &d) = default;
 
 	/**
-	 * \brief Default move constructor.
+	 * Default move constructor.
 	 * \param d
 	 */
 	derived(derived &&d) = default;
 
 	/**
-	 * \brief Default copy assignment operator.
+	 * Default copy assignment operator.
 	 * \param d
 	 * \return
 	 */
 	derived &operator=(const derived &d) = default;
 
 	/**
-	 * \brief Default move assignment operator.
+	 * Default move assignment operator.
 	 * \param d
 	 * \return
 	 */
 	derived &operator=(derived &&d) = delete;
 
 	/**
-	 * \brief Implemented function
+	 * Implemented function
 	 */
 	void init() override;
 };
