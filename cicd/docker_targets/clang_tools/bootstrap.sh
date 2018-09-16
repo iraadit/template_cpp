e1d94f4bdd1ea1c6535a3cab776c21144ed024db
Melvin Vermeeren <mail@mel.vin>
2018-09-16 22:45:34 +0200

    cicd: switch to debian stable for clang_tools img
    Also symlink before pkg removal for consistency with other bootstrap
    logic. Image version is bumped and YAML adjusted to reflect this.

--- cicd/docker_targets/clang_tools/bootstrap.sh
+++ cicd/docker_targets/clang_tools/bootstrap.sh
@@ -2,18 +2,23 @@
 
 CATCH=2.4.0
 CLANG=6.0
+DEBIAN=stretch
 
 set -e
+
+printf '%s\n' \
+	"deb http://ftp.debian.org/debian $DEBIAN-backports main" \
+	> /etc/apt/sources.list.d/$DEBIAN-backports.list
 
 apt-get update
 
 apt-get install --no-install-recommends -y \
 	binutils \
 	ca-certificates \
-	clang-$CLANG \
-	clang-format-$CLANG \
-	clang-tidy-$CLANG \
-	cmake \
+	clang-$CLANG/$DEBIAN-backports \
+	clang-format-$CLANG/$DEBIAN-backports \
+	clang-tidy-$CLANG/$DEBIAN-backports \
+	cmake/$DEBIAN-backports \
 	curl \
 	git \
 	make
@@ -23,14 +28,14 @@ curl -Lf \
 	-o /usr/local/include/catch2/catch.hpp \
 	https://git.mel.vin/mirror/catch/raw/v$CATCH/catch2/catch.hpp
 
+ln -s /usr/bin/clang-$CLANG /usr/local/bin/cc
+ln -s /usr/bin/clang++-$CLANG /usr/local/bin/c++
+ln -s /usr/bin/clang-format-$CLANG /usr/local/bin/clang-format
+ln -s /usr/bin/clang-tidy-$CLANG /usr/local/bin/clang-tidy
+
 apt-get purge -y \
 	curl
 
 apt-get autoremove -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-
-ln -s /usr/bin/clang-$CLANG /usr/local/bin/cc
-ln -s /usr/bin/clang++-$CLANG /usr/local/bin/c++
-ln -s /usr/bin/clang-format-$CLANG /usr/local/bin/clang-format
-ln -s /usr/bin/clang-tidy-$CLANG /usr/local/bin/clang-tidy
